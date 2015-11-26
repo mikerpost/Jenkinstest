a9b5183d7ae7b8ee0f02fe88fbc2d236cf805a90
Michael Robinson <mike.robinson.dev@gmail.com>
2015-11-25 22:25:10 -0700

    used pycharm.

--- test/tests.py
+++ test/tests.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
-#import random
+# import random
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-class SimpleTest(unittest.TestCase):
-"""
-Test 
-    """
 
+class SimpleTest(unittest.TestCase):
     @unittest.skip("demonstrating skipping")
     def test_skipped(self):
-    """
-    test_skipped
-    """
         self.fail("shouldn't happen")
 
     def test_pass(self):
-    """
-    test_pass
-    """
         self.assertEqual(10, 7 + 3)
 
-    # def test_fail(self):
-    #     self.assertEqual(11, 7 + 3)
+        # def test_fail(self):
+        #     self.assertEqual(11, 7 + 3)
